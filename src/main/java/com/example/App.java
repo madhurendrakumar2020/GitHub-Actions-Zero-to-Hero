53a029c1e77d788b866fcb2e63b26f81e34f4ff7
madhurendrakumar2020 <74235421+madhurendrakumar2020@users.noreply.github.com>
2026-04-03 20:57:40 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Running app");
-        System.out.println("Running app2");
+        System.out.println("Running app21");
     }
 }
